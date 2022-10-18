526af3acf540c5f59bd6acd57bcba1ba474e8870
dhanu Pandey <dhanu.Pandey@BRILLIO.COM>
2022-10-18 12:15:17 +0530

    Deploy

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Foodshala App",
+    subtitle: "Django",
+    description:
+      "Foodshala app is food ordering app where user can add food items in food cart and place the order",
+    image: "./foodshala.png",
+    link: "https://github.com/dhanupandey12/foodshala1",
+  },
+  {
+    title: "Facebase Login System",
+    subtitle: "Flask and Python",
+    description:
+      "When the app wll recognise the face of user, they can login to the website",
+    image: "./img1.gif",
+    link: "https://github.com/dhanupandey1/face_recognition",
+  },
+  {
+    title: "Videostream App",
+    subtitle: "React and Node",
+    description:
+      "You can stream videos here",
+    image: "./videostream.jfif",
+    link: "https://github.com/dhanupandey/Videostreamapp",
+  },
+  {
+    title: "Blog App",
+    subtitle: "Node.js",
+    description:
+      "User can write and publish their blog",
+    image: "./Blog.jfif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Bakend Devloper",
+    image: "https://randomuser.me/api/portraits/men/39.jpg",
+    name: "Airbus",
+    company: "Infosys",
+  },
+  {
+    quote:
+      "Fullstack developer",
+    image: "https://randomuser.me/api/portraits/men/39.jpg",
+    name: "Silicon Valley Bank",
+    company: "Brillio Technology",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node.js",
+  "Express",
+  "MongoDb",
+  "Angular.js",
+];
